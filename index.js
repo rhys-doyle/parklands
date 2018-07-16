7f8721c0458b52f8a9600037ee6829cc1384d5e1
Rhys Doyle <rhys.doyle@gmail.com>
2018-07-16 12:06:29 +1000

    toggling between playIcon and displayed text

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ $(document).ready(() => {
     $("body").animate({ scrollTop: $path }, 1000);
   });
 });
+
+var playHeader = document.querySelector(".playHeader");
+var playSpan = document.querySelector(".playSpan");
+var playIcon = document.querySelector(".playIcon");
+var playBox = document.querySelector(".play");
+
+$(".playA").hover(
+  () => {
+    playIcon.style.display = "none";
+    playSpan.style.display = "block";
+    playHeader.style.display = "block";
+    playBox.style.padding = "0 0 5.5rem 0";
+  },
+  () => {
+    playIcon.style.display = "block";
+    playSpan.style.display = "none";
+    playHeader.style.display = "none";
+    playBox.style.padding = "0 0 5.1rem 0";
+  }
+);
